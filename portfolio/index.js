a3be8e44494ef1298fc779f34c0fce821ef5bea5
Artem Potlov <potlov.artem@gmail.com>
2022-01-16 22:43:48 +0300

    feat: add function to change header styles

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,8 @@
-console.log('Init project');
+const header = document.querySelector('.header');
+window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+        header.classList.add('header_active');
+    } else {
+        header.classList.remove('header_active');
+    }
+};
