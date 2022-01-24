6b36f62f4194f70adec87739d264e3b0abdf508e
Artem Potlov <potlov.artem@gmail.com>
2022-01-24 12:35:51 +0300

    feat: add adaptive navigation menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,5 @@
 console.log('My total points: 110');
+
 const header = document.querySelector('.header');
 window.onscroll = () => {
     if (window.pageYOffset > 50) {
@@ -7,3 +8,23 @@ window.onscroll = () => {
         header.classList.remove('header_active');
     }
 };
+
+const hamburger = document.querySelector('.header__hamburger');
+const headerNav = document.querySelector('.header__nav');
+const headerNavLinks = document.querySelector('.header__list');
+const headerNavLinksItem = document.querySelectorAll('.header__item');
+const toggleMenu = () => {
+  if (!hamburger.classList.contains('open')) {
+    hamburger.classList.add('open');
+    headerNav.classList.add('open');
+    headerNavLinks.classList.add('open');
+
+  } else {
+    hamburger.classList.remove('open');
+    headerNav.classList.remove('open');
+    headerNavLinks.classList.remove('open');
+  }
+
+};
+hamburger.addEventListener('click', toggleMenu);
+headerNavLinksItem.forEach((el) => el.addEventListener('click', toggleMenu));
